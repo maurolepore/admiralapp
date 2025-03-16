7156bb4c2588d3a9fa4ae64f99aa1df062bf0b9a
Mauro Lepore <maurolepore@gmail.com>
2025-03-16 02:35:09 +0000

    Style

--- R/run_app.R
+++ R/run_app.R
@@ -10,24 +10,25 @@ run_app <- function() {
     data = teal_data(reference = admiralapp::public),
     modules = modules(
       # Example creating a module
-      "Reference" |> module(
-        server = function(input, output, session, data) {
-          output$ref <- DT::renderDT({
-            DT::datatable(data()[["reference"]], escape = FALSE)
-          })
-        },
-        ui = function(id, ...) {
-          ns <- shiny::NS(id)
-          DT::DTOutput(ns("ref"))
-        }
-      ),
+      "Reference" |>
+        module(
+          server = function(input, output, session, data) {
+            output$ref <- DT::renderDT({
+              DT::datatable(data()[["reference"]], escape = FALSE)
+            })
+          },
+          ui = function(id, ...) {
+            ns <- shiny::NS(id)
+            DT::DTOutput(ns("ref"))
+          }
+        ),
       # Example reusing a module
       "Raw" |> teal::example_module()
     )
   ) |>
     modify_title("admiralverse") |>
     modify_header(tags$p("The toy ", maurolepore("admiralverse"))) |>
-    modify_footer(  tags$p("Created with love and ", maurolepore("dverse")))
+    modify_footer(tags$p("Created with love and ", maurolepore("dverse")))
 
   shiny::shinyApp(app$ui, app$server)
 }
--- app.R
+++ app.R
@@ -1,10 +1,7 @@
 options(
-  "teal.bs_theme" =  bslib::bs_theme(version = "3", bootswatch = "united")
+  "teal.bs_theme" = bslib::bs_theme(version = "3", bootswatch = "united")
 )
 
 # ShinyApp
 pkgload::load_all()
 run_app()
-
-
-
