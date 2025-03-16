8b5d9cf93deb63a341a6d2812a59570749a4d49c
Mauro Lepore <maurolepore@gmail.com>
2025-03-16 02:52:09 +0000

    Style

--- R/reference_data_table_module.R
+++ R/reference_data_table_module.R
@@ -1,10 +1,10 @@
 reference_data_table_module <- function(label = "Reference") {
-    module(
-      label = label,
-      server = reference_data_table_server,
-      ui = reference_data_table_ui,
-      datanames = "reference"
-    )
+  module(
+    label = label,
+    server = reference_data_table_server,
+    ui = reference_data_table_ui,
+    datanames = "reference"
+  )
 }
 reference_data_table_server <- function(id, data) {
   shiny::moduleServer(id, function(input, output, session) {
